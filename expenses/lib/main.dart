22ffb46397eda4de9fb5a16353d28196ce4e6406
lenard <lenard.eiffe@gmail.com>
2021-11-18 20:13:33 +1030

    Expenses tutorial app initial check in

---
+++ expenses/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: const Center(
+        child: Text('Widget Playground'),
+      ),
+    );
+  }
+}
