a0db5599ca9bb3c26a737b1f022e49145b0c3eeb
lenard <lenard.eiffe@gmail.com>
2021-11-18 20:44:57 +1030

    Lesson 5

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -24,9 +24,22 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: const Center(
-        child: Text('Widget Playground'),
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+              ),
+            ),
+            Card(
+              color: Colors.red,
+              child: Text('List of TX'),
+            )
+          ]),
     );
   }
 }
